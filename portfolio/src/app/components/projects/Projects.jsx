13fc0943f02ca21360da316bafd0d09cb99829ef
Orianamartina <orianapettinelli04@gmail.com>
2023-06-27 13:25:09 -0300

    justified text and atered margins

--- portfolio/src/app/components/projects/Projects.jsx
+++ portfolio/src/app/components/projects/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects(){
     const electroShop = {
         img: electroShop1,
         name: "ElectroShop",
-        description: "E-Commerce focused on electronic devices such as computers, tablets, cell-phones as well as accesories. Created as a bootcamp final project with a group of five students using SCRUM methodologies.",
+        description: "E-Commerce focused on electronic devices such as computers, tablets, cell-phones as well as accesories. Created as a bootcamp final project with a group of five students using SCRUM methodologies. Made with React and nodeJs",
         url: "https://electroshop-delta.vercel.app",
         video: "https://www.youtube.com/watch?v=hz_tKs5abKA",
         repositorio: "https://github.com/Orianamartina/Electroshop"
@@ -22,7 +22,7 @@ export default function Projects(){
     const sudoku = {
         img: sudoku1,
         name: "Sudoku",
-        description: "Sudoku SPA made in React. Accessible for wider screens, interactive interface, keyboard support, game state management, different styles depending on the user's preference.",
+        description: "Sudoku SPA made in React. Accessible for wider screens, interactive interface, keyboard support, game state management, different styles depending on the user's preference. Made with React and nodeJs",
         url: "https://sudoku-prueba.vercel.app",
         repositorio:"https://github.com/Orianamartina/sudoku-prueba"
     }
@@ -30,14 +30,14 @@ export default function Projects(){
     const sudokuMobile = {
         img: sudoku2,
         name: "Sudoku Mobile",
-        description: "Sudoku SPA made in react. Accessible for smaller screens, interactive interface with number selection buttons for touch screens, game state management, different styles depending on the user's preference.",
+        description: "Sudoku SPA made in react. Accessible for smaller screens, interactive interface with number selection buttons for touch screens, game state management, different styles depending on the user's preference. Made with React and nodeJs",
         url: "https://sudoku-prueba.vercel.app",
         repositorio: "https://github.com/Orianamartina/sudoku-prueba"
     }
     const dogHub = {
         img: dogHub1,
         name: "DogHub",
-        description: "Dog breed searching SPA. Responsive. Search your favorite breeds, sort them or filter them by the given options, find all their information, or create a new one.",
+        description: "Dog breed searching SPA. Responsive. Search your favorite breeds, sort them or filter them by the given options, find all their information, or create a new one. Made with React and nodeJs",
         url: "https://proyecto-individual-henry.vercel.app",
         repositorio: "https://github.com/Orianamartina/Proyecto-individual-henry"
     }
