ee09fa3c211ad138dbcbcdacbc59cbe6f96714f4
Orianamartina <orianapettinelli04@gmail.com>
2023-06-27 12:37:35 -0300

    icons

--- portfolio/src/app/components/contact/Contact.jsx
+++ portfolio/src/app/components/contact/Contact.jsx
@@ -8,22 +8,30 @@ export default function Contact(){
     return(
         <div className={style.container}>
             
-            <div className={style.textContainers}>
-                <img src={linkedin.src} className={style.buttons}  alt="" />
-               <a className={style.linkText}  href="https://www.linkedin.com/in/oriana-pettinelli-777a09268/" target="_blank">Linkedin</a> 
-            </div>
-            <div className={style.textContainers}>
-               <img src={github.src} className={style.buttons} alt="" />
-                <a className={style.linkText} href="https://github.com/Orianamartina" target="_blank">Github</a>
-            </div>
-            <div className={style.textContainers}>
-                <img src={phone.src} className={style.buttons} alt="" />
-                <h2  className={style.linkText} >+54 011 4989 6418</h2>
-            </div>
-            <div className={style.textContainers}>
-                <img src={email.src} className={style.buttons} alt="" />
-                <h2 className={style.linkText} >orianapettinelli1@gmail.com</h2>
-            </div>
+            <button className={style.textContainers}>
+                
+               <a className={style.linkText}  href="https://www.linkedin.com/in/oriana-pettinelli-777a09268/" target="_blank">
+                <img src={linkedin.src} className={style.buttonImage}  alt="" />
+                Linkedin</a> 
+            </button>
+            <button className={style.textContainers}>
+               
+                <a className={style.linkText} href="https://github.com/Orianamartina" target="_blank">
+                    <img src={github.src} className={style.buttonImage} alt="" />
+                    Github</a>
+            </button>
+            <button className={style.textContainers}>
+       
+                <h2  className={style.linkText} >
+                    <img src={phone.src} className={style.buttonImage} alt="" />   
+                    +54 011 4989 6418</h2>
+            </button>
+            <button className={style.textContainers}>
+                
+                <h2 className={style.linkText} >
+                <img src={email.src} className={style.buttonImage} alt="" />    
+                    orianapettinelli1@gmail.com</h2>
+            </button>
             
            
         </div>
