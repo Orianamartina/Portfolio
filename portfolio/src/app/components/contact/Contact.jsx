63173b1f3b8005c601bdb39b102a3445c9d1b4be
Orianamartina <orianapettinelli04@gmail.com>
2023-06-26 21:24:59 -0300

    email and phone icon

--- portfolio/src/app/components/contact/Contact.jsx
+++ portfolio/src/app/components/contact/Contact.jsx
@@ -1,25 +1,27 @@
 import style from "./contact.module.css"
 import github from "../../../../public/github.png"
 import linkedin from "../../../../public/linkedin.png"
+import phone from "../../../../public/phone-call.png"
+import email from "../../../../public/email.png"
 export default function Contact(){
  
     return(
         <div className={style.container}>
             
             <div className={style.textContainers}>
-                
-               <a className={style.linkText}  href="https://www.linkedin.com/in/oriana-pettinelli-777a09268/"          target="_blank">
                 <img src={linkedin.src} className={style.buttons}  alt="" />
-                Linkedin</a> 
+               <a className={style.linkText}  href="https://www.linkedin.com/in/oriana-pettinelli-777a09268/" target="_blank">Linkedin</a> 
             </div>
             <div className={style.textContainers}>
                <img src={github.src} className={style.buttons} alt="" />
-                <a className={style.linkText} href="https://github.com/Orianamartina"          target="_blank">Github</a>
+                <a className={style.linkText} href="https://github.com/Orianamartina" target="_blank">Github</a>
             </div>
             <div className={style.textContainers}>
+                <img src={phone.src} className={style.buttons} alt="" />
                 <h2  className={style.linkText} >+54 011 4989 6418</h2>
             </div>
             <div className={style.textContainers}>
+                <img src={email.src} className={style.buttons} alt="" />
                 <h2 className={style.linkText} >orianapettinelli1@gmail.com</h2>
             </div>
             
