6e186e76dbcca07dcd29158449a247faa3d3f62c
orianamartina <oriana@camba.coop>
2025-02-20 23:40:43 -0300

    wip: v2

--- portfolio/src/app/components/navBar/NavBar.jsx
+++ portfolio/src/app/components/navBar/NavBar.jsx
@@ -1,30 +1,30 @@
-import style from "./navBar.module.css"
-export default function NavBar(props){
+import style from "./navBar.module.css";
+export default function NavBar(props) {
+  const selectedComponent = props.selectedComponent;
+  const components = props.components;
+  //technology stack, contact
 
-    const selectedComponent = props.selectedComponent
-    const components = props.components
-      //technology stack, contact
-
-    return(
-        <div className={style.container}>
-            
-                {components.map((component) => {
-                        return(
-                        <>
-                            
-                            <button className={selectedComponent === component.id ? style.active : style.notActive}
-                                
-                                    onClick={() => props.handleComponentClick(component.id)}
-                                    key={component.id}
-                                    >
-                                <img src={component.image} className={style.buttonImage}></img>
-                                <h1 className={style.buttonText}>{component.name}</h1>
-                            </button>
-                        
-                        </>
-                   )
-                })}
-        
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      {components.map((component) => {
+        return (
+          <>
+            <button
+              className={
+                selectedComponent === component.id
+                  ? style.active
+                  : style.notActive
+              }
+              onClick={() => props.handleComponentClick(component.id)}
+              key={component.id}
+            >
+              {component.image}
+              {/* <img src={component.image} className={style.buttonImage}></img> */}
+              <h1 className={style.buttonText}>{component.name}</h1>
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
 }
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,36 +1,32 @@
-'use client'
-import style from './page.module.css'
-import Home from './components/home/Home'
-import NavBar from './components/navBar/NavBar'
-import Projects from './components/projects/Projects'
-import AboutMe from './components/about me/AboutMe'
-import { useState } from 'react'
-import {motion, AnimatePresence } from 'framer-motion'
-import Experience from './components/Stack/Experience'
-import Contact from './components/contact/Contact'
-import heart from "../../public/heart.png"
-import book from "../../public/open-book.png"
-import home from "../../public/home.png"
-import portfolio from "../../public/portfolio.png"
-import phone from "../../public/phone-call.png"
+"use client";
+import style from "./page.module.css";
+import NavBar from "./components/navBar/NavBar";
+import { useState, Suspense, lazy } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import heart from "../../public/heart.png";
+import book from "../../public/open-book.png";
+import home from "../../public/home.png";
+import portfolio from "../../public/portfolio.png";
+import phone from "../../public/phone-call.png";
+import { HomeIcon } from "./components/Icons/HomeIcon";
+
+const Home = lazy(() => import("./components/home/Home"));
+const AboutMe = lazy(() => import("./components/about me/AboutMe"));
+const Projects = lazy(() => import("./components/projects/Projects"));
+const Experience = lazy(() => import("./components/Stack/Experience"));
+const Contact = lazy(() => import("./components/contact/Contact"));
 
 export default function Main() {
+  const [selectedComponent, setSelectedComponent] = useState("home");
 
-  const [selectedComponent, setSelectedComponent] = useState('home');
-
-
-  const handleComponentClick = (componentId)=>{
+  const handleComponentClick = (componentId) => {
     setSelectedComponent(componentId);
-
-  }
+  };
 
   const transitionVariants = {
     enter: {
-      opacity: 0,
-      //x: 50,
-      overflow: 'hidden',
-
-      
+      x: 50,
+      overflow: "hidden",
     },
     center: {
       opacity: 1,
@@ -38,54 +34,61 @@ export default function Main() {
     },
     exit: {
       opacity: 0,
-     // x: -50,
+      x: -50,
     },
   };
-  
+
   const renderComponent = (componentId) => {
     switch (componentId) {
-      case 'home':
+      case "home":
         return <Home />;
-      case 'aboutMe':
+      case "aboutMe":
         return <AboutMe />;
-      case 'projects':
+      case "projects":
         return <Projects />;
-      case 'experience':
-        return <Experience />
-      case 'contact':
-        return <Contact />
+      case "experience":
+        return <Experience />;
+      case "contact":
+        return <Contact />;
       default:
         return null;
     }
   };
 
-     const components = [
-        { name: 'Home', id: 'home', image:home.src },
-        { name: 'About Me', id: 'aboutMe', image:heart.src },
-        { name: 'Projects', id: 'projects', image: portfolio.src },
-        { name: 'Experience', id: 'experience', image:book.src},
-        { name: 'Contact', id: 'contact', image: phone.src},
-      ];
+  const components = [
+    { name: "Home", id: "home", image: <HomeIcon fill={"#000"} /> },
+    { name: "About Me", id: "aboutMe", image: heart.src },
+    { name: "Projects", id: "projects", image: portfolio.src },
+    { name: "Experience", id: "experience", image: book.src },
+    { name: "Contact", id: "contact", image: phone.src },
+  ];
   return (
-    <main >
-      <NavBar handleComponentClick={handleComponentClick} selectedComponent={selectedComponent} components={components}/>
+    <main>
+      <NavBar
+        handleComponentClick={handleComponentClick}
+        selectedComponent={selectedComponent}
+        components={components}
+      />
       <AnimatePresence>
         {components.map((component) => (
           <motion.div
             key={component.id}
             variants={transitionVariants}
             initial="enter"
-            animate={selectedComponent === component.id ? 'center' : 'exit'}
+            animate={selectedComponent === component.id ? "center" : "exit"}
             exit="exit"
             onExitComplete={() => window.scrollTo(0, 0)}
             className={style.motionDiv}
             transition={{ duration: 1 }}
           >
-            {selectedComponent === component.id && renderComponent(component.id)}
+            {selectedComponent === component.id && (
+              <Suspense fallback={<div>Loading...</div>}>
+                {renderComponent(component.id)}
+              </Suspense>
+            )}
           </motion.div>
         ))}
       </AnimatePresence>
-      
     </main>
-  )
+  );
 }
